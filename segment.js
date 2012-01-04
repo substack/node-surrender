cc45a482f6fefd6467d59b68f0197c1760c09f65
James Halliday <mail@substack.net>
2012-01-03 17:38:50 -0800

    smoother line algorithm

---
+++ segment.js
@@ -0,0 +1,41 @@
+var charm = require('charm')(process);
+charm.reset();
+
+charm.on('^C', function () {
+    charm.cursor(true);
+    process.exit(0)
+});
+
+function at (x, y, c) {
+    charm.position(Math.floor(x), Math.floor(y));
+    charm.write(c);
+}
+
+var m = 10.0;
+var b = -2;
+
+for (var y = 1; y < 24; y++) {
+    // y = m * x + b
+    // x = (y - b) / m
+    var xp = Math.floor((y - 1 - b) / m);
+    var xn = Math.floor((y - b) / m);
+    
+    var chars =
+        m > 1 ? [ '|', '\\' ] :
+        m > 0.5 ? [ '\\', '_' ] :
+        m > 0 ? [ '\\', '_' ] :
+        m < 1 ? [ '|', '/' ] :
+        m < 0 ? [ '/', '_' ] :
+        []
+    ;
+    
+    if (m > 1) {
+        at(xn - 1, y, xp === xn ? chars[0] : chars[1]);
+    }
+    else {
+        at(xp, y, chars[0]);
+        for (var x = xp + 1; x < xn; x++) {
+            at(x, y, chars[1]);
+        }
+    }
+}
